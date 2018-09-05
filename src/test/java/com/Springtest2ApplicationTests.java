8bf5bebfe4300003c1f6821ac70f65430891023a
huangmou1996 <1013788672@qq.com>
2018-09-05 12:37:25 +0800

    黄某测试测试类

--- src/test/java/com/Springtest2ApplicationTests.java
+++ src/test/java/com/Springtest2ApplicationTests.java
@@ -1,16 +1,29 @@
 package com;
 
+import com.pojo.Music;
+import com.service.MusicService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springtest2ApplicationTests {
 
+    @Resource
+    private MusicService musicService;
+
+
     @Test
     public void contextLoads() {
+
+        for (Music m: musicService.getMusics()
+             ) {
+            System.out.println(m.getId()+"   "+m.getMusicName());
+        }
     }
 
 }
